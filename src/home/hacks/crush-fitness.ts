b1f25c7b21c9cc255dafc4ed8ba010b96af85a2f
Zachary Mace <zcmace@gmail.com>
2025-04-14 14:45:59 -0400

    GWHW proof of concept done for crush-fitness, next step is to generalize misc.ts and crush-fitness.ts into 1 scaled script that takes in a target.

---
+++ src/home/hacks/crush-fitness.ts
@@ -0,0 +1,35 @@
+import { NS } from '@ns';
+
+export async function main(ns: NS) {
+  const flags = ns.flags([['target', '']]);
+  const host = flags['target'] as string;
+  ns.tprint(host);
+  const hackThreads = 148;
+  const hackWeakThreads = 6;
+  const growThreads = 131;
+  const growWeakThreads = 11;
+  const hackWait = 352540;
+  const weak1Wait = 500;
+  const growWait = 95011;
+  const weak2Wait = 1500;
+  const target = (flags['target'] || 'crush-fitness') as string;
+  while (true) {
+    const hackPid = ns.run('/hacks/hack.js', hackThreads, '--target', target, '--wait', hackWait);
+    const weak1Pid = ns.run('/hacks/weaken.js', hackWeakThreads, '--target', target, '--wait', weak1Wait);
+    const growPid = ns.run('/hacks/grow.js', growThreads, '--target', target, '--wait', growWait);
+    const weak2Pid = ns.run('/hacks/weaken.js', growWeakThreads, '--target', target, '--wait', weak2Wait);
+
+    if (hackPid === 0 || weak1Pid === 0 || growPid === 0 || weak2Pid === 0) {
+      ns.kill(hackPid);
+      ns.kill(weak1Pid);
+      ns.kill(growPid);
+      ns.kill(weak2Pid);
+      ns.tprint('Failed to start 1 or more HWGW scripts. Stopping execution.');
+      ns.exit();
+    }
+
+    while (ns.isRunning(hackPid) || ns.isRunning(weak1Pid) || ns.isRunning(growPid) || ns.isRunning(weak2Pid)) {
+      await ns.sleep(1000);
+    }
+  }
+}
--- src/home/misc.ts
+++ src/home/misc.ts
@@ -1,8 +1,120 @@
 import { NS } from '@ns';
 
 export async function main(ns: NS) {
-  const flags = ns.flags([['h', '']]);
-  const host = flags['h'] as string;
+  ns.print(ns.formatNumber(ns.getPurchasedServerUpgradeCost('home-2', 256)));
+  const target = ns.getServer(ns.args[0] as string);
+  const player = ns.getPlayer();
+  const home = ns.getServer();
+  const formula = ns.formulas.hacking;
+  const minDiff = ns.getServerMinSecurityLevel(target.hostname);
+  const currDiff = ns.getServerSecurityLevel(target.hostname);
 
-  ns.tprint(ns.getPurchasedServerCost(256));
+  //times
+  const hackTime = formula.hackTime(target, player);
+  const weakenTime = formula.weakenTime(target, player);
+  const growTime = formula.growTime(target, player);
+
+  //prep
+  const weakenThreads = (currDiff - minDiff) / 0.05;
+  const growThreadsPrep = formula.growThreads(target, player, ns.getServerMaxMoney(target.hostname), 2);
+  const growSecOffset = growThreadsPrep * 0.004;
+  const weakenSecOffsetGrowPrep = growSecOffset / 0.05;
+
+  // The below is correct in general but should be tested.
+  // Grow to Weaken threads proportion is 2 weaken thread per 25 grow threads
+  // Hack to weaken threads proportion is 1 weaken thread per 25 hack threads
+
+  //Runner Limitations
+  ns.tprintf('\nThis Server Thread Maximums: \n');
+  const hackMem = ns.getScriptRam('/hacks/hack.js');
+  const growMem = ns.getScriptRam('/hacks/grow.js');
+  const maxHackThreads = Math.floor(home.maxRam / ns.getScriptRam('/hacks/hack.js'));
+  const maxGrowThreads = Math.floor(home.maxRam / ns.getScriptRam('/hacks/grow.js'));
+  ns.tprintf('Max Hack Threads: %s at %s gb per thread', maxHackThreads, hackMem);
+  ns.tprintf('Max Grow/Weaken Threads: %s at %s gb per thread', maxGrowThreads, growMem);
+
+  ns.tprintf('\nPrep Threads: \n');
+  ns.tprintf('WeakenThreads to prep grow: %s', weakenThreads);
+  ns.tprintf('GrowThreads to prep: %s', growThreadsPrep);
+  ns.tprintf('WeakenThreads to mitigate grow prep: %s', weakenSecOffsetGrowPrep);
+
+  const amountToHack = ns.getServerMaxMoney(target.hostname) * 0.1;
+  const temp = target.moneyAvailable;
+  target.moneyAvailable = ns.getServerMaxMoney(target.hostname);
+  const hackThreadsFor10Perc = Math.ceil(0.1 / formula.hackPercent(target, player));
+  const hackOffset = hackThreadsFor10Perc * 0.002;
+  const weakenThreadsToOffsetHack = hackOffset / 0.05;
+  target.moneyAvailable = target.moneyAvailable * 0.9;
+  const growThreadsToOffsetHack = formula.growThreads(target, player, ns.getServerMaxMoney(target.hostname), 2);
+  const weakenThreadsToOffsetGrow = Math.ceil((growThreadsToOffsetHack * 0.004) / 0.05);
+  target.moneyAvailable = temp;
+  const hackWait = weakenTime - hackTime;
+  const firstWeakenWait = 500;
+  const growWait = weakenTime - growTime + 1000;
+  const finalWeakenWait = 1500;
+
+  ns.tprintf('\nStep times: \n');
+  ns.tprintf(
+    'Hack Time: %s | Offset: %s | Total: %s',
+    formatTime(hackTime),
+    formatTime(hackWait),
+    formatTime(hackTime + hackWait),
+  );
+  ns.tprintf(
+    'Weaken #1 Time: %s | Offset: %s | Total: %s',
+    formatTime(weakenTime),
+    formatTime(firstWeakenWait),
+    formatTime(weakenTime + firstWeakenWait),
+  );
+  ns.tprintf(
+    'Grow Time: %s | Offset: %s | Total: %s',
+    formatTime(growTime),
+    formatTime(growWait),
+    formatTime(growTime + growWait),
+  );
+  ns.tprintf(
+    'Weaken #2 Time: %s | Offset: %s | Total: %s',
+    formatTime(finalWeakenWait),
+    formatTime(hackWait),
+    formatTime(weakenTime + finalWeakenWait),
+  );
+
+  ns.tprintf(
+    '\nOffsets:\nHack: \t\t%s\nWeaken #1: \t%s\nGrow: \t\t%s\nWeaken #2: \t%s',
+    Math.ceil(hackWait),
+    firstWeakenWait,
+    Math.ceil(growWait),
+    finalWeakenWait,
+  );
+
+  ns.tprintf(
+    '\nThreads: \nHack: \t\t%s\nWeaken #1: \t%s\nGrow: \t\t%s\nWeaken #2: \t%s\nTotal: \t\t%s',
+    Math.ceil(hackThreadsFor10Perc),
+    Math.ceil(weakenThreadsToOffsetHack),
+    Math.ceil(growThreadsToOffsetHack),
+    Math.ceil(weakenThreadsToOffsetGrow),
+    Math.ceil(hackThreadsFor10Perc + weakenThreadsToOffsetHack + growThreadsToOffsetHack + weakenThreadsToOffsetGrow),
+  );
+
+  ns.tprintf(
+    '\nRAM Usage: \nHack: \t\t%s\nWeaken #1: \t%s\nGrow: \t\t%s\nWeaken #2: \t%s\nTotal: \t\t%s',
+    Math.ceil(hackThreadsFor10Perc) * hackMem,
+    Math.ceil(weakenThreadsToOffsetHack) * growMem,
+    Math.ceil(growThreadsToOffsetHack) * growMem,
+    Math.ceil(weakenThreadsToOffsetGrow) * growMem,
+    Math.ceil(hackThreadsFor10Perc) * hackMem +
+      Math.ceil(weakenThreadsToOffsetHack) * growMem +
+      Math.ceil(growThreadsToOffsetHack) * growMem +
+      Math.ceil(weakenThreadsToOffsetGrow) * growMem,
+  );
+
+  ns.tprintf('Hack Chance: %s', ns.formulas.hacking.hackChance(target, player));
+}
+
+function formatTime(timeInMs: number) {
+  const hoursPart: number = Math.floor(timeInMs / (1000 * 60 * 60));
+  const minutesPart: number = Math.floor((timeInMs % (1000 * 60 * 60)) / (1000 * 60));
+  const secPart: number = Math.floor((timeInMs % (1000 * 60)) / 1000);
+  const msPart: number = timeInMs % 1000;
+  return `${hoursPart}:${minutesPart}:${secPart}.${Math.ceil(msPart)}`;
 }
